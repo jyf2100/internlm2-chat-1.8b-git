5a2a405b8cfdd6891b6788dc70d3a97a77a4fdb7
Roc Ju <dvs@vip.sina.com>
2024-08-07 17:05:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 from openxlab.model import download
 
-base_path = './internlm2-chat-1.8b'
+base_path = './internlm2-chat-1.8B'
 os.system(f'git clone https://code.openxlab.org.cn/jyf2100/internlm2-1.8B.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
